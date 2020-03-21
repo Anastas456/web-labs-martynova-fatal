4bebbdee8efddd0eb3612659a753a55ad671bec4
Anastas456 <wild456west@gmail.com>
2020-03-21 21:01:38 +0300

    svdhv

---
+++ Laba3/animation3.js
@@ -0,0 +1,9 @@
+var cat = anime({
+    targets: '#eye',
+    translateX:-10,
+    translateY:5,
+    direction: 'alternate',
+    loop: true,
+    easing: 'easeInOutSine',
+    duration: 2000
+  });
